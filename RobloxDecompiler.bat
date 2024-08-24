75575f40f4cd3b56f595fd9751860c3d9f9087a1
Jay <148450689+jayjanse@users.noreply.github.com>
2024-08-24 20:51:23 +0200

    RobloxDecompiler.bat
    Hello here is a new decompiler if make it

---
+++ RobloxDecompiler.bat
@@ -0,0 +1,32 @@
+@echo off
+:1
+color b
+echo hello, i will help you to steal games its free.
+echo first join a game
+echo then type decom to start
+echo done.
+set /p input=
+if /i %input%==decom goto decompiling
+if /i %input%==exit goto exit
+if /i not %input%== decom,exit goto 1
+
+:Decompiling
+echo start decompiling...
+
+:Done
+timeout 4
+echo steal a map with scripts...
+timeout 4
+echo creating a folder named decompiledgame...
+timeout 4
+echo done.
+echo if created a folder named decompiledgame.
+pause
+C:\User\i2500k>D:
+
+D:\>DecompiledGame.rbxl Decompiledgame.rbxl
+
+
+
+
+
